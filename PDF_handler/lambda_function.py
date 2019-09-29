072e97eff9c61eafeb2c8032d0635609e52ca926
digodk <diogodklein@gmail.com>
2019-09-29 20:54:19 -0300

    Adicionada função para iniciar textract async, mas está sendo rejeitada pela aws por algum motivo obscuro

--- PDF_handler/lambda_function.py
+++ PDF_handler/lambda_function.py
@@ -4,7 +4,9 @@ import requests
 from boto3 import client
 
 TELEGRAM_TOKEN = os.environ['BotToken']
-URL = 'https://api.telegram.org/bot{}/'.format(TELEGRAM_TOKEN)
+BOT_URL = 'https://api.telegram.org/bot{}/'.format(TELEGRAM_TOKEN)
+FILE_URL = 'https://api.telegram.org/file/bot{}/'.format(TELEGRAM_TOKEN)
+BUCKET_NAME = os.environ['BucketName']
 
 
 def lambda_handler(event, context):
@@ -14,19 +16,43 @@ def lambda_handler(event, context):
     file_name = document['file_name']
 
     # Requisita o link do arquivo para o telegram
-    url = URL + 'getFile?file_id={}'.format(file_id)
+    url = BOT_URL + 'getFile?file_id={}'.format(file_id)
     response = requests.get(url).json()
     file_path = response['result']['file_path']
 
     # Requisita o arquivo para o telegram
-    url = URL + file_path
-    file = requests.get(url, stream=True).content
+    url = FILE_URL + file_path
+    file = requests.get(url).content
 
     # Salva o arquivo no bucket
     s3_client = client('s3')
-    bucket_name = os.environ['BucketName']
-    s3_client.put_object(Body=file, Bucket=bucket_name, Key=file_name)
+    s3_client.put_object(Body=file, Bucket=BUCKET_NAME, Key=file_name)
 
     # Solicita ao textract análise do documento
+    textract_client = client('textract')
 
-    # Grava mensagem na pilha SQS
+    doc_location = {
+        'S3Object': {
+            'Bucket': BUCKET_NAME,
+            'Name': file_name
+        }
+    }
+    feature_types = [
+        "TABLES",
+        "FORMS"
+    ]
+
+    sns_topic_arn = os.environ['TextractSNSTopicARN']
+    role_arn = os.environ['TextractSNSRoleARN']
+    job_tag = file_name
+    notification_channel = {
+        'RoleArn': role_arn,
+        'SNSTopicArn': sns_topic_arn
+    }
+
+    response = textract_client.start_document_analysis(
+        DocumentLocation=doc_location,
+        FeatureTypes=feature_types,
+        JobTag=job_tag,
+        NotificationChannel=notification_channel
+    )
