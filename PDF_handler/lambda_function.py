928ee9776d72e8168bf5ea9d81fb456b08bff6bc
digodk <diogodklein@gmail.com>
2019-09-30 01:40:13 -0300

    refactoring bug fixes envio de mensagem para fila SQS após chamada do textract

--- PDF_handler/lambda_function.py
+++ PDF_handler/lambda_function.py
@@ -9,12 +9,7 @@ FILE_URL = 'https://api.telegram.org/file/bot{}/'.format(TELEGRAM_TOKEN)
 BUCKET_NAME = os.environ['BucketName']
 
 
-def lambda_handler(event, context):
-    message = json.loads(event['Records'][0]['Sns']['Message'])
-    document = message['document']
-    file_id = document['file_id']
-    file_name = document['file_name']
-
+def get_telegram_file(file_id):
     # Requisita o link do arquivo para o telegram
     url = BOT_URL + 'getFile?file_id={}'.format(file_id)
     response = requests.get(url).json()
@@ -22,14 +17,18 @@ def lambda_handler(event, context):
 
     # Requisita o arquivo para o telegram
     url = FILE_URL + file_path
-    file = requests.get(url).content
+    return requests.get(url).content
 
-    # Salva o arquivo no bucket
+
+def salvar_arquivo_s3(file, file_name):
     s3_client = client('s3')
     s3_client.put_object(Body=file, Bucket=BUCKET_NAME, Key=file_name)
 
-    # Solicita ao textract análise do documento
+
+def iniciar_analise_textract(file_name):
     textract_client = client('textract')
+    sns_topic_arn = os.environ['TextractSNSTopicARN']
+    role_arn = os.environ['TextractSNSRoleARN']
 
     doc_location = {
         'S3Object': {
@@ -37,22 +36,51 @@ def lambda_handler(event, context):
             'Name': file_name
         }
     }
+
     feature_types = [
-        "TABLES",
-        "FORMS"
+        'TABLES',
+        'FORMS'
     ]
 
-    sns_topic_arn = os.environ['TextractSNSTopicARN']
-    role_arn = os.environ['TextractSNSRoleARN']
-    job_tag = file_name
     notification_channel = {
         'RoleArn': role_arn,
         'SNSTopicArn': sns_topic_arn
     }
 
-    response = textract_client.start_document_analysis(
+    return textract_client.start_document_analysis(
         DocumentLocation=doc_location,
         FeatureTypes=feature_types,
-        JobTag=job_tag,
         NotificationChannel=notification_channel
     )
+
+
+def publicar_mensagem_sqs(file_name, job_id):
+    sqs_queue_url = os.environ['SQSQueueTextractURL']
+    sqs_client = client('sqs')
+    message_body = {
+        'job_id': job_id,
+        'file_name': file_name
+    }
+    client.send_message(
+        QueueUrl=sqs_queue_url,
+        MessageBody=json.dumps(message_body)
+    )
+
+
+def lambda_handler(event, context):
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+    document = message['document']
+    file_id = document['file_id']
+    file_name = document['file_name']
+
+    # Faz o download do arquivo
+    file = get_telegram_file(file_id)
+
+    # Salva o arquivo no bucket
+    salvar_arquivo_s3(file, file_name)
+    del file
+
+    # Solicita ao textract análise do documento
+    response = iniciar_analise_textract(file_name)
+
+    # Publica a mensagem na fila do SQS
