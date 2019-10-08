169065257da495f344d1af248f93694b92d3390b
digodk <diogodklein@gmail.com>
2019-10-07 23:13:03 -0300

    Refeita a função para encontrar o header da prova

--- textract_handler/lambda_function.py
+++ textract_handler/lambda_function.py
@@ -39,90 +39,75 @@ def lambda_handler(event, context):
     # Extrai lista de blocos
     blocks = response['Blocks']
 
-    # Extrai tabelas
-    tables = [block for block in blocks if block['BlockType'] == 'TABLE']
+    # Extrai blocos de palavras
+    word_blocks = [block for block in blocks if block['BlockType'] == 'WORD']
 
-    # TODO Estender totalmente tabelas em colunas e linhas contendo blocos de células
-    # TODO Identificar tabela de informações da prova
-    # TODO Converter blocos de célula em textos concatenados
-    # TODO Concatenar todos os textos por coluna
+    # Extrai blocos de células
+    cell_blocks = [block for block in blocks if block['BlockType'] == 'CELL']
 
-    # Extrai blocos de célula que contém texto
-    tables_rels_list = [[rel_list for rel_list in table['Relationships']] for table in tables]
+    # Extrai blocos de tabelas
+    table_blocks = [block for block in blocks if block['BlockType'] == 'TABLE']
 
-    tables_childs_ids = [
-        [relationship['Ids'] for relationship in rel_list]
-        for rel_list in tables_rels_list
-    ]
+    # Carrega as palavras em células com texto
+    def list_cell_words(cell):
+        words = []
+        if 'Relationships' in cell:
+            for rel in cell['Relationships']:
+                for child_id in rel['Ids']:
+                    block = list(filter(lambda block: block['Id'] == child_id, word_blocks))
+                    if len(block) > 0:
+                        words.append(block[0]['Text'])
+        cell['Words'] = words
+        return cell
 
-    tables_childs_cells = [
-        [block for block in blocks if block['Id'] in child_ids and block['BlockType'] == 'CELL']
-        for relationships in tables_childs_ids for child_ids in relationships
-    ]
+    cells_and_texts = list(map(list_cell_words, cell_blocks))
 
-    cells_with_text = [
-        [cell for cell in child_list if 'Relationships' in cell] for child_list in tables_childs_cells
-    ]
+    # Lista células com a palavra 'Nome'
+    filtered_cells = list(filter(lambda cell: 'Nome' in cell['Words'], cells_and_texts))
 
-    # Extrai e lista textos das células
-    cells_rels_list = [
-        [
-            [rel_list for rel_list in cell['Relationships']]
-            for cell in table
-        ] for table in cells_with_text
-    ]
+    # Caso haja mais de uma célula, então seleciona aquela que está mais acima na página
+    if len(filtered_cells) > 1:
+        filtered_cells = list(filter(lambda cell: cell['Page'] == 1, filtered_cells))
+        filtered_cells = sorted(filtered_cells, key=lambda cell: cell['Geometry']['BoundingBox']['Top'])
 
-    cells_childs_ids = [
-        [[relationship['Ids'] for relationship in cell]
-         for cell in table
-         ] for table in cells_rels_list
-    ]
+    # Registra célula que pertence a tabela de descrição
+    name_cell = filtered_cells[0]
 
-    cells_child_blocks = [
-        [
-            [
-                [block for block in blocks if block['Id'] in child_ids]
-                for relationship in cell for child_ids in relationship
-            ] for cell in table
-        ] for table in cells_childs_ids
-    ]
+    # Encontra a tabela de informações pela célula de nome
+    def find_table_by_cell(table):
+        if 'Relationships' in table:
+            for rel in table['Relationships']:
+                return name_cell['Id'] in rel['Ids']
 
-    cells_text_blocks = [
-        [
-            [block for block in child_blocks if 'Text' in block]
-            for cell in table
-            for child_blocks in cell
-        ] for table in cells_child_blocks
-    ]
+    info_table = list(filter(find_table_by_cell, table_blocks))[0]
 
-    cells_texts = [
-        [
-            [block['Text'] for block in cell]
-            for cell in table
-        ] for table in cells_text_blocks
-    ]
+    # Carrega as células com texto da tabela
+    def load_table_cells(table):
+        result = []
+        for rel in table['Relationships']:
+            for child_id in rel['Ids']:
+                cell = list(filter(lambda cell: cell['Id'] == child_id, cells_and_texts))[0]
+                result.append(cell)
+        return result
 
-    # Montar texto da tabela
-    tables_texts = [
-        [': '.join(text_list) for text_list in table] for table in cells_texts
-    ]
+    info_cells = load_table_cells(info_table)
 
-    # Identificar nome e número da prova
+    # Carrega os dados em um dicionário
+    def find_info_data(info):
+        # Identifica a célula com o texto procurado e carrega toda a coluna
+        temp_cell = list(filter(lambda cell: info in cell['Words'], info_cells))[0]
+        cells = list(filter(lambda cell: cell['ColumnIndex'] == temp_cell['ColumnIndex'], info_cells))
+        # Necessário considerar os casos onde as linhas vêm mescladas em uma célula por erro do textract
+        if len(cells) == 1:
+            result = ''.join(list(filter(lambda word: word != info, temp_cell['Words'])))
+            result = result.replace(info, '')
+        # E os casos onde o texto vem em células distintas, nesse caso necessário excluir a célula de cabeçalho
+        else:
+            cells = list(filter(lambda cell: cell['Id'] != temp_cell['Id']))
+            result = ''.join(list(map(lambda cell: ''.join(cell['Words'], cells))))
+        return result.strip()
 
-    prova_number = ''.join([
-        cell_text[1]
-        for table in cells_texts for cell_text in table
-        if 'Prova' in cell_text
-    ])
-
-    prova_name = ''.join([
-        cell_text[1]
-        for table in cells_texts for cell_text in table
-        if 'Nome' in cell_text
-    ])
-
-    # Envia uma mensagem com o nome e número da prova
-    # Essa etapa é temporária, apenas para debug, por isso o chat_id harcoded
-    message = f'Nome da prova: {prova_name} \n' \
-              f'Número da prova: {prova_number}'
-    send_message(message, '54337384')
+    header = dict(Nome=find_info_data('Nome'),
+                  Prova=find_info_data('Prova'),
+                  HoraLancamento=' '.join([find_info_data('Data Lancamento'), find_info_data('Hora Lancamento')]),
+                  HoraEntrega=' '.join([find_info_data('Data Entrega'), find_info_data('Hora Entrega')]))
